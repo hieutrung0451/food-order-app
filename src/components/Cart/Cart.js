1a708630f19ff055631c79ea01680f9a2de87051
trunghieu <trunghieu0451@gmail.com>
2023-02-22 16:01:48 +0700

    add Context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const ctx = useContext(CartContext);
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {ctx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
